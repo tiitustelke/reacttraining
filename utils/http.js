34ebb535f609864d71b8a1cb21f52154bbb00d7f
Tiitus Telke <tiitus.telke@metropolia.fi>
2021-08-31 12:39:56 +0300

    fix stuff not loading

--- utils/http.js
+++ utils/http.js
@@ -1,10 +1,11 @@
 const doFetch = async (url, options) => {
-  const response = fetch(url, options);
-  const json = response.json();
-  if(json.error) {
+  console.log(url);
+  const response = await fetch(url, options);
+  const json = await response.json();
+  if (json.error) {
     throw new Error(json.message + ': ' + json.error);
   } else if (!response.ok) {
-    throw new Error('fetch failed :(');
+    throw new Error('fetch failed');
   } else {
     return json;
   }
